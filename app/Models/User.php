a5b0aeac48d1faac882b343369ad16a3bff5b6a1
meblo98 <lomouhamedelbachir@gmail.com>
2025-01-27 12:23:57 +0000

    config jwt sur le model usre

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -19,6 +20,17 @@ class User extends Authenticatable
      */
     protected $guarded = [];
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+   
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
